ba1860eda3a1e9f39701a9aec3e731e7ea9b6717
Ben Galloway <ben@bengalloway.io>
2019-01-22 22:57:50 +0000

    Add logos and typography

--- src/components/CecilianAppBar.js
+++ src/components/CecilianAppBar.js
@@ -3,11 +3,20 @@ import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import logo from "../assets/logo.png";
 
 const styles = {
     root: {
         flexGrow: 1,
     },
+    appTitle: {
+        fontFamily: "Brawler, serif"
+    },
+    logo: {
+        width: "20%",
+        maxWidth: "50px",
+        marginRight: "1rem"
+    }
 };
 
 const CecilianAppBar = ({ classes }) => {
@@ -15,7 +24,8 @@ const CecilianAppBar = ({ classes }) => {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" color="inherit">
+                    <img src={logo} alt="The Cecilian Archive logo" className={classes.logo}/>
+                    <Typography variant="h6" color="inherit" className={classes.appTitle}>
                         Archive Uploader
                     </Typography>
                 </Toolbar>

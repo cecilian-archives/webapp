27ff7f4f5515e776788bfac2074a761cecabebfd
Ben Galloway <git@bengalloway.io>
2020-05-28 19:25:23 +0100

    Add routes for new developments
    Update required dependencies. Bump minor version.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ import CecilianAppBar from "./components/CecilianAppBar";
 import CecilianLogosFooter from "./components/CecilianLogosFooter";
 import StartPage from "./components/root/StartPage";
 import MinutesPage from "./components/root/MinutesPage";
+import PhotosPage from "./components/root/PhotosPage";
 import PalettePage from "./components/root/PalettePage";
 import TempScanPage from "./components/root/TempScanPage";
 import DevPage from "./components/root/DevPage";
+import PrototypeDemo from "./components/root/PrototypeDemo";
+import LoginPage from "./components/root/LoginPage";
 
 const App = () => (
   <Router>
@@ -20,9 +23,12 @@ const App = () => (
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/minutes" element={<MinutesPage />} />
+        <Route path="/photos" element={<PhotosPage />} />
         <Route path="/palette" element={<PalettePage />} />
         <Route path="/scangen" element={<TempScanPage />} />
         <Route path="/dev" element={<DevPage />} />
+        <Route path="/prototype" element={<PrototypeDemo />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>

f0793c7c78cc5a9799059ba8047b72b64f44044b
Ben Galloway <git@bengalloway.io>
2020-07-01 01:33:32 +0100

    Sketch out routing structure
    Redirect old upload subdomain to new path.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Outlet,
   Navigate,
 } from "react-router-dom";
 import CecilianAppBar from "./components/CecilianAppBar";
@@ -21,14 +22,29 @@ const App = () => (
     <CecilianAppBar />
     <div style={{ flexGrow: "1" }}>
       <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/minutes" element={<MinutesPage />} />
-        <Route path="/photos" element={<PhotosPage />} />
-        <Route path="/palette" element={<PalettePage />} />
-        <Route path="/scangen" element={<TempScanPage />} />
-        <Route path="/people" element={<PeopleManagementPage />} />
-        <Route path="/prototype" element={<PrototypeDemo />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Outlet />} />
+        <Route path="search" element={<Outlet />} />
+        <Route path="browse" element={<Outlet />} />
+        <Route path="items" element={<Outlet />}>
+          <Route path="/" element={<Navigate to="prototype" />} />
+          <Route path="prototype" element={<PrototypeDemo />} />
+        </Route>
+        <Route path="people" element={<Outlet />} />
+        <Route path="collections" element={<Outlet />} />
+        <Route path="upload" element={<Outlet />}>
+          <Route path="/" element={<StartPage />} />
+          <Route path="minutes" element={<MinutesPage />} />
+          <Route path="photos" element={<PhotosPage />} />
+        </Route>
+        <Route path="admin" element={<Outlet />}>
+          <Route path="palette" element={<PalettePage />} />
+          <Route path="scangen" element={<TempScanPage />} />
+          <Route path="people" element={<PeopleManagementPage />} />
+        </Route>
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="auth" element={<Navigate to="/login" />} />
+        <Route path="account" element={<Navigate to="/people/me" />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
